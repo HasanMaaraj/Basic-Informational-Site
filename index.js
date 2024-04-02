a2d0ed0b4bf92f88484e63e84d137de20321bf6e
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2024-04-02 14:37:22 +0300

    Edit variable declarations

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
         const filePath = './index.html';
-        responseStatus = 200;
+        const responseStatus = 200;
     } else if (req.url === '/about') {
         const filePath = './about.html';
-        responseStatus = 200;
+        const responseStatus = 200;
     } else if (req.url === '/contact-me') {
         const filePath = './contact-me.html';
-        responseStatus = 200;
+        const responseStatus = 200;
     } else {
         const filePath = './404.html';
-        responseStatus = 404;
+        const responseStatus = 404;
     }
 
-    fs.readFile(filePath, (error, data) => {
+    fs.readFileSync(filePath, (error, data) => {
         if (error) {
             res.writeHead(500, { 'Content-Type': 'text/html' });
             res.end('500 Internal server error');
