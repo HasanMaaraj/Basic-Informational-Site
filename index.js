55397c66031d4bbe8225befd6346f5d70fd294a0
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2024-04-02 15:16:52 +0300

    Get the response options through directory

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 const http = require('http');
 const fs = require('fs');
 
+const getResponseOptions = (url) => {
+    let filePath = undefined;
+    let statusCode = undefined;
+
+    if (url === '/') {
+        filePath = './index.html';
+        statusCode = 200;
+    } else if (url === '/about') {
+        filePath = './about.html';
+        statusCode = 200;
+    } else if (url === '/contact-me') {
+        filePath = './contact-me.html';
+        statusCode = 200;
+    } else {
+        filePath = './404.html';
+        statusCode = 404;
+    }
+    return [filePath, statusCode];
+}
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        const filePath = './index.html';
-        const responseStatus = 200;
-    } else if (req.url === '/about') {
-        const filePath = './about.html';
-        const responseStatus = 200;
-    } else if (req.url === '/contact-me') {
-        const filePath = './contact-me.html';
-        const responseStatus = 200;
-    } else {
-        const filePath = './404.html';
-        const responseStatus = 404;
-    }
 
-    fs.readFileSync(filePath, (error, data) => {
+    const [filePath, statusCode] = getResponseOptions(req.url)
+
+    fs.readFile(filePath, (error, data) => {
         if (error) {
             res.writeHead(500, { 'Content-Type': 'text/html' });
             res.end('500 Internal server error');
         }
-        res.writeHead(responseStatus, {'content-type': 'text/html'})
+        res.writeHead(statusCode, {'content-type': 'text/html'})
         res.write(data)
         res.end()
     })
