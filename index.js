d955d93fc76166e528c9bcc6ae6654391377f333
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2024-04-02 14:02:44 +0300

    Send error page response

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const server = http.createServer((req, res) => {
     } else if (req.url === '/contact-me') {
         const filePath = './contact-me.html';
         responseStatus = 200;
+    } else {
+        const filePath = './404.html';
+        responseStatus = 404;
     }
     fs.readFile(filePath, (error, data) => {
         res.writeHead(responseStatus, {'content-type': 'text/html'})
