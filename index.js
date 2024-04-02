5f0b1c8ba0a6ce13ecd90d62baecc7bf565148a3
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2024-04-02 13:38:55 +0300

    Send about page response

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const server = http.createServer((req, res) => {
     if (req.url === '/') {
         const filePath = './index.html';
         responseStatus = 200;
+    } else if (req.url === '/about') {
+        const filePath = './about.html';
+        responseStatus = 200;
     }
     fs.readFile(filePath, (error, data) => {
         res.writeHead(responseStatus, {'content-type': 'text/html'})
